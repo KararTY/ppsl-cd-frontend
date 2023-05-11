39f7dce06e07cf00694fd7af8b7e59aff8bcaedb
KararTY <alremahykarar@ymail.com>
2023-05-11 17:53:03 +0200

    submodule it

--- src/components/editors/components/placeholder.jsx
+++
@@ -1,7 +0,0 @@
-export function Placeholder ({ children }) {
-  return (
-    <div className="pointer-events-none absolute left-0 top-0 select-none overflow-hidden text-ellipsis p-3">
-      {children}
-    </div>
-  )
-}
--- src/components/editors/plugins/EntityImage/Image.jsx
+++
@@ -1,7 +0,0 @@
-function ImageC (props) {
-  return <img {...props} className="h-full w-full object-contain" />
-}
-
-const ImageComponent = (props) => <ImageC {...props} />
-
-export default ImageComponent
