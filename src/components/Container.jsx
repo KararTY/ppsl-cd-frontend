c23fb2c796de1a2681505c4dec58ca012cb931fc
KararTY <alremahykarar@ymail.com>
2023-05-15 15:59:03 +0200

    New header

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,12 +3,18 @@ import { Header } from './Header'
 
 export function Container ({ className = '', header = {}, children }) {
   return (
-    <main className="container">
-      <article className={`m-0 px-0 max-sm:pt-0 ${className}`}>
-        <Header {...header} />
-        {children}
-        <Footer />
-      </article>
-    </main>
+    <>
+      <div className="sticky top-0 border-b-slate-950 border-opacity-25 bg-white p-2 leading-none shadow-lg dark:border-b dark:bg-[#11191f]">
+        <nav className="container">
+          <Header {...header} className="w-full" />
+        </nav>
+      </div>
+      <main className="container !pt-0">
+        <article className={`m-0 px-0 pt-0 ${className} rounded-t-none`}>
+          {children}
+          <Footer />
+        </article>
+      </main>
+    </>
   )
 }
