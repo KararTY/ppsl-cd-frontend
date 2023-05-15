c52888d84f6b080a74f3b0ce02922649b86f8cb7
KararTY <alremahykarar@ymail.com>
2023-05-15 16:10:07 +0200

    add more heading padding

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,7 +4,7 @@ import { Header } from './Header'
 export function Container ({ className = '', header = {}, children }) {
   return (
     <>
-      <div className="sticky top-0 border-b-slate-950 border-opacity-25 bg-white p-2 leading-none shadow-lg dark:border-b dark:bg-[#11191f] dark:bg-gradient-to-r dark:from-[#18232c]">
+      <div className="sticky top-0 border-b-slate-950 border-opacity-25 bg-white py-4 leading-none shadow-lg dark:border-b dark:bg-[#11191f] dark:bg-gradient-to-r dark:from-[#18232c]">
         <nav className="container">
           <Header {...header} className="w-full" />
         </nav>
